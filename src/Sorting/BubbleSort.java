acafbef3f1d5d784413302605b3e5c9d6d049067
Sarthak Jain <Sarthak Jain@10.12.9.130>
2022-09-22 13:51:29 +0530

    Bubble sort

--- src/Sorting/BubbleSort.java
+++ src/Sorting/BubbleSort.java
@@ -25,7 +25,7 @@ public class BubbleSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] data = { 3,9,13,90,22,57 };
+		int[] data = { 4,9,13,90,22,57 };
 	    
 	    // call method using class name
 	    BubbleSort.bubbleSort(data);
